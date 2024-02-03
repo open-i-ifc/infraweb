0cd66e9cc233c6bec15fe4c7df00dc2b159232c9
MateuszMazurek <mailing.bot@gmail.com>
2024-02-03 13:34:07 +0100

    added openbim-components

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { IFCViewer, ViewerProvider } from './react-components/ifcViewer';
+import * as Router from "react-router-dom"
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    <Router.BrowserRouter>
+      <ViewerProvider>
+      
+      <Router.Routes>
+        <Router.Route path="/ifc" element={<IFCViewer />}></Router.Route>
+        <Router.Route path="/" element={<IFCViewer />}></Router.Route>
+        
+      </Router.Routes>
+      </ViewerProvider>
+    </Router.BrowserRouter>
+  </>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ app/src/react-components/ifcViewer.tsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+
+import * as OBC from "openbim-components"
+
+interface Props {
+    
+}
+
+interface IViewerContext {
+    viewer: OBC.Components | null,
+    setViewer: (viewer: OBC.Components | null) => void
+}
+
+export const ViewerContext = React.createContext<IViewerContext>({
+    viewer: null, 
+    setViewer: () => {}
+})
+
+export function ViewerProvider (props: {children: React.ReactNode}) {
+    const [viewer, setViewer] = React.useState<OBC.Components | null>(null)
+    return (
+        <ViewerContext.Provider value={{
+            viewer,setViewer
+        }} >
+            { props.children }
+        </ViewerContext.Provider >
+    )
+}
+
+
+
+
+export function IFCViewer(props: Props) {
+    return <> 
+    InfraWeb
+    </>
+}
