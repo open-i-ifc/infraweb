d9d39a54814123dc58eafb849f73cbcea0c58269
MateuszMazurek <mailing.bot@gmail.com>
2024-02-03 13:43:49 +0100

    added basic IFC Viewer

--- app/src/react-components/ifcViewer.tsx
+++ app/src/react-components/ifcViewer.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 import * as OBC from "openbim-components"
 
 interface Props {
@@ -31,7 +30,65 @@ export function ViewerProvider (props: {children: React.ReactNode}) {
 
 
 export function IFCViewer(props: Props) {
-    return <> 
-    InfraWeb
-    </>
+
+    const { setViewer } = React.useContext(ViewerContext)
+    let viewer: OBC.Components
+    let scene
+
+    const createViewer = async () => {
+        viewer = new OBC.Components()
+        setViewer(viewer)
+        
+        const sceneComponent = new OBC.SimpleScene(viewer)
+        sceneComponent.setup()
+        viewer.scene = sceneComponent
+        scene = sceneComponent.get()
+        scene.background = null
+        const viewerContainer = document.getElementById("viewer-container") as HTMLDivElement
+        //const rendererComponent = new OBC.PostproductionRenderer(viewer, viewerContainer)
+        const rendererComponent = new OBC.SimpleRenderer(viewer, viewerContainer)
+
+        const renderer = rendererComponent.get()
+        console.log(renderer)
+
+        renderer.setPixelRatio( window.devicePixelRatio );
+        
+        
+        viewer.renderer = rendererComponent as unknown as OBC.SimpleRenderer
+        
+        const cameraComponent = new OBC.OrthoPerspectiveCamera(viewer)
+        viewer.camera = cameraComponent
+    
+        const raycasterComponent = new OBC.SimpleRaycaster(viewer)
+        viewer.raycaster = raycasterComponent
+        
+        const grid = new OBC.SimpleGrid(viewer);
+        renderer.shadowMap.enabled = true;
+
+
+        viewer.init()
+
+
+
+        
+    }
+
+    viewer = new OBC.Components()
+    React.useEffect(() => {
+        createViewer()
+        
+        return () => {
+            
+            viewer.dispose()
+            setViewer(null)
+        }
+      }, [])
+
+    return (
+        <div
+            id="viewer-container"
+            className="dashboard-card"
+            style={{ minWidth: 0, position: "relative", height: "100vh"}}
+        />
+    )
 }
