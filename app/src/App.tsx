8d9ea3ccf77f92d485764f7f385c61b4b6a80dcf
MateuszMazurek <mailing.bot@gmail.com>
2024-02-03 13:21:00 +0100

    React deployment with TS

---
+++ app/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/opensource-construction/infraweb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          InfraWeb
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
