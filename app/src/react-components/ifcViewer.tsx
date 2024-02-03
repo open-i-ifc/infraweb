5bdd35608bc6de9b4c0e8cf361c94bad7b740796
Szaby <veress_szabolcs@yahoo.com>
2024-02-03 17:40:32 +0100

    section plane added

--- app/src/react-components/ifcViewer.tsx
+++ app/src/react-components/ifcViewer.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import * as React from "react"
 import * as OBC from "openbim-components"
+import * as THREE from "three"
 import { AlignTool } from "../openbim-tools/align"
 import { FragmentsGroup } from "bim-fragment"
 interface Props {
@@ -92,14 +93,39 @@ export function IFCViewer(props: Props) {
 
 
         ifcLoader.onIfcLoaded.add(async (model) => {
+            console.log(model)
             for (const fragment of model.items) { culler.add(fragment.mesh) }
             propertiesProcessor.process(model)
-            highlighter.events.select.onHighlight.add((selection) => {
-                const fragmentID = Object.keys(selection)[0]
-                const expressID = Number([...selection[fragmentID]][0])
-                propertiesProcessor.renderProperties(model, expressID)
-            })
-            highlighter.update()
+                
+                const shapeFill = new THREE.MeshBasicMaterial({color: 'lightgray', side: 2});
+                const shapeLine = new THREE.LineBasicMaterial({ color: 'black' });
+                const shapeOutline = new THREE.MeshBasicMaterial({color: 'black', opacity: 0.2, side: 2, transparent: true});
+                const meshes = viewer.meshes
+                console.log(meshes)
+                console.log(meshes.length)
+                
+                clipper.styles.create('White shape, black lines', new Set(meshes), shapeLine, shapeFill, shapeOutline);
+                
+                /* Each Shape different Color
+                for(let i=0; i++; i <= meshes.length){
+                    const mesh = meshes[i]
+                    console.log(mesh)
+                    const materials = mesh.material as THREE.Material[]
+                    const material = materials[0] as THREE.MeshLambertMaterial
+                    const color = material.color
+                    const shapeFill = new THREE.MeshBasicMaterial({color: "white", side: 2});
+                    console.log(shapeFill)
+                    const shapeLine = new THREE.LineBasicMaterial({ color: color });
+                    console.log(shapeLine)
+                    const shapeOutline = new THREE.MeshBasicMaterial({color: color, opacity: 0.2, side: 2, transparent: true});
+                    console.log(shapeOutline)
+                    clipper.styles.create(`${i.toString}`, new Set([mesh]), shapeLine, shapeFill, shapeOutline);
+
+                }
+                */
+           
+            culler.needsUpdate = true
+            onModelLoaded(model)
             culler.needsUpdate = true
             onModelLoaded(model)
         })
@@ -122,6 +148,8 @@ export function IFCViewer(props: Props) {
         const clipper = new OBC.EdgesClipper(viewer);
 
         clipper.enabled = true;
+        viewerContainer.ondblclick = () => clipper.create();
+
 
 
 
